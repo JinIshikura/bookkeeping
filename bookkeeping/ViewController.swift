823a40cb24a9fdf1d2abb4b6b4fcdce8fe2a73e6
石藏仁 <6305byjin@gmail.com>
2019-05-23 22:23:21 +0900

    Initial Commit

---
+++ bookkeeping/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bookkeeping
+//
+//  Created by 石藏　仁 on 2019/05/23.
+//  Copyright © 2019 JiN!. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
