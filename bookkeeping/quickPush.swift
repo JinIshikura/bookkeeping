91b12ba3f2f377ca8b6f3643ff86445e0e48b0e3
Misppy <6305byjin@gmail.com>
2019-06-18 18:16:43 +0900

    五要素の動作を一部変更なるも、swiftによるviewの表示にエラーあり。

--- bookkeeping/quickPush.swift
+++ bookkeeping/quickPush.swift
@@ -10,23 +10,37 @@ class quickPush: UIViewController {
     @IBOutlet var sihon: UIButton!
     @IBOutlet var sonota: UIButton!
     @IBOutlet var score:UILabel!
+    @IBOutlet var numberIndication:UILabel!
     var audioPlayer: AVAudioPlayer!
+    
     var questionRandom:Int=0
     var answer:Int=0
     var push :Int=0
-    var shisanArray:[String] = ["現金","売掛金","当座預金"]
-    var husaiArray: [String] = ["買掛金","借入金","当座借越"]
-    var hiyouArray:[String] = ["旅費","消耗品費","雑費"]
-    var syuekiArray:[String] = ["商品売買益","受取利息","受取家賃"]
-    var shihonArray:[String] = ["資本金"]
-    var otherArray:[String] = ["現金過不足","貸倒引当金","引出金"]
+    var questionNumber:Int = 0
     
+    var shisanUsesArray:[String] = ["現金","売掛金","当座預金","商品","建物","備品","機械","車両運搬具","土地","貸付金","繰越商品","立替金","銀行預金","小口現金","受取手形","有価証券","手形貸付金","未収入金","前払金","支払手付金","仮払金","前払費用","未収収益","消耗品","他店商品券"]
+    var husaiUsesArray: [String] = ["買掛金","借入金","当座借越"]
+    var hiyouUsesArray:[String] = ["旅費","消耗品費","雑費"]
+    var syuekiUsesArray:[String] = ["商品売買益","受取利息","受取家賃"]
+    var shihonUses:[String] = ["資本金"]
+    var otherUsesArray:[String] = ["現金過不足","貸倒引当金","引出金"]
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        numberIndication.text = String(questionNumber + 1) + "問目"
         makeQuestion()
+        
+        /****************************************
+        *選択した勘定科目だけが出題されるようにしたい。
+        ****************************************/
+//        shisanUsesArray = shisanFArray
+//        husaiUsesArray = husaiFArray
+//        hiyouUsesArray = hiyouFArray
+//        syuekiUsesArray = syuekiFArray
+//        shihonUses = shihonF
+//        otherUsesArray = otherFArray
     }
     
     override func didReceiveMemoryWarning() {
@@ -66,25 +80,33 @@ class quickPush: UIViewController {
     }
     
     func makeQuestion(){
-        //questionRandom = Int.random(in: 0 ... shisanArray.count-1)
+        var a:Int = 0
         answer = Int.random(in: 0 ... 5)
+        
         if(answer == 0){
-            questionRandom = Int.random(in: 0 ... shisanArray.count - 1)
-            questionLabel.text = shisanArray[questionRandom]
+            a = shisanUsesArray.count - 1
+            questionRandom = Int.random(in: 0 ... a)
+            questionLabel.text = shisanUsesArray[questionRandom]
         }else if(answer == 1){
-            questionRandom = Int.random(in: 0 ... husaiArray.count - 1)
-            questionLabel.text = husaiArray[questionRandom]
+            a = husaiUsesArray.count - 1
+            questionRandom = Int.random(in: 0 ... a)
+            questionLabel.text = husaiUsesArray[questionRandom]
         }else if(answer == 2){
-            questionRandom = Int.random(in: 0 ... hiyouArray.count - 1)
-            questionLabel.text = hiyouArray[questionRandom]
+            a = hiyouUsesArray.count - 1
+            questionRandom = Int.random(in: 0 ... a)
+            questionLabel.text = hiyouUsesArray[questionRandom]
         }else if(answer == 3){
-            questionRandom = Int.random(in: 0 ... syuekiArray.count - 1)
-            questionLabel.text = syuekiArray[questionRandom]
+            a = syuekiUsesArray.count - 1
+            questionRandom = Int.random(in: 0 ... a)
+            questionLabel.text = syuekiUsesArray[questionRandom]
         }else if(answer == 4){
-            questionLabel.text = shihonArray[0]
+            a = syuekiUsesArray.count - 1
+            questionRandom = Int.random(in: 0 ... a)
+            questionLabel.text = syuekiUsesArray[questionRandom]
         }else{
-            questionRandom = Int.random(in: 0 ... otherArray.count - 1)
-            questionLabel.text = otherArray[questionRandom]
+            a = otherUsesArray.count - 1
+            questionRandom = Int.random(in: 0 ... a)
+            questionLabel.text = otherUsesArray[questionRandom]
         }
     }
     func scoreing(){
@@ -92,6 +114,11 @@ class quickPush: UIViewController {
             score.text = "正解"
         }else{
             score.text = "不正解"
+        }
+        questionNumber += 1
+        numberIndication.text = String(questionNumber + 1) + "問目"
+        if questionNumber >= 3{
+            self.performSegue(withIdentifier: "goyousoAnswer", sender: nil)
         }
         return
     }
